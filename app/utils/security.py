0000411edfec33380d28637d83ca2b6c9f998665
jinhui niu <jinhuiniu@jinhuideMacBook-Pro.local>
2024-10-10 11:05:02 +0800

    update

--- app/utils/security.py
+++ app/utils/security.py
@@ -1,4 +1,17 @@
 import hashlib
+from cryptography.fernet import Fernet
 
 def hash_password(password, salt):
     return hashlib.md5((password + salt).encode('utf-8')).hexdigest()
+
+def encrypt_words(key,words):
+    """加密文件名"""
+    cipher = Fernet(key)
+    encrypted_words = cipher.encrypt(words.encode())
+    return encrypted_words.decode()
+
+def decrypt_words(key,encrypted_words):
+    """解密文件名"""
+    cipher = Fernet(key)
+    decrypted_words = cipher.decrypt(encrypted_words.encode())
+    return decrypted_words.decode()
